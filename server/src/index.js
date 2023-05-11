4378f2637b5232d4882251c9c560e53284dd0a9b
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2023-05-11 23:45:17 +0530

    dep

--- server/src/index.js
+++ server/src/index.js
@@ -63,7 +63,11 @@ new CronJob(
   "Asia/Kolkata"
 );
 app.get('/', (req, res) => {
-  res.send('This is our Gharwala khana web api web App api');
+  // res.send('This is our Gharwala khana web api web App api');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
 });
 
 // app.use("/api/v1/user", user);
