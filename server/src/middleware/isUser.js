3d43debfa70add0be29cfad01b0789d63be31a7f
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2023-05-12 17:43:47 +0530

    dep

--- server/src/middleware/isUser.js
+++ server/src/middleware/isUser.js
@@ -1,14 +1,14 @@
 const userModel = require('../models/user')
 const jwt = require('jsonwebtoken')
-exports.isUser = async (req,res,next) =>{    
-    try{
-        const {userToken} = req.cookies;
-        if(!userToken) return res.status(500).json({success:false});
-    
+exports.isUser = async (req, res, next) => {
+    try {
+        const { userToken } = req.cookies;
+        if (!userToken) return res.status(500).json({ success: false, message: "usertoken not present cookie" });
+
         const decodeData = jwt.verify(userToken, process.env.SECRET_KEY);
         req.user = await userModel.findById(decodeData.id);
         next();
-        }catch(error){
-            return res.status(500).json({success:false,message:error.message});
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
     }
 }
